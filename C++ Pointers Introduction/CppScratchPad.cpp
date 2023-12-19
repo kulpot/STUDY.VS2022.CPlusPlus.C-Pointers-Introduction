21e99b0a4102665c06bb201aad40044fcc625281
Kulpot <kulpotwahu@gmail.com>
2023-12-19 10:44:58 +0800

    Add project files.

---
+++ C++ Pointers Introduction/CppScratchPad.cpp
@@ -0,0 +1,12 @@
+#include <cassert>
+#include <iostream>
+using std::cout;
+using std::end;
+
+// ----------------------------------- C++ Pointers Introduction --------------------------------------------
+// ref link:https://www.youtube.com/watch?v=lAHgavXXWXM&list=PLRwVmtr-pp05AW78n3IkMJ3M1dlQ-wgv1
+
+void main()
+{
+
+}

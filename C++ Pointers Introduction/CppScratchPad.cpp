f67ae349ee9b98dda3353661d73cef1e8792d1e5
Kulpot <kulpotwahu@gmail.com>
2023-12-19 10:47:00 +0800

    ...

--- C++ Pointers Introduction/CppScratchPad.cpp
+++ C++ Pointers Introduction/CppScratchPad.cpp
@@ -6,6 +6,8 @@ using std::end;
 // ----------------------------------- C++ Pointers Introduction --------------------------------------------
 // ref link:https://www.youtube.com/watch?v=lAHgavXXWXM&list=PLRwVmtr-pp05AW78n3IkMJ3M1dlQ-wgv1
 
+// C++ predecessor to java and C#
+
 void main()
 {
 
